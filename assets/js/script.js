43633bc99072ffb309d661bb7c19950734698399
Lexi Geller <leximoconner@yahoo.com>
2023-05-07 16:29:34 -0400

    added timer to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+var timerEl = document.getElementById('timer');
+// Timer that counts down from 75
+function timer() {
+    var timeLeft = 75;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 0
+      if (timeLeft > -1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = 'Time: ' + timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else {
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+  }
+
+timer();
+
 //set interval for the timer
 //event listeners for all buttons
 //store highscores in local storages
