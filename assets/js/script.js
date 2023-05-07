58718eb355377773564cd359b0ebe15ef517c183
Lexi Geller <leximoconner@yahoo.com>
2023-05-07 14:58:46 -0400

    completed basic HTML, added some style to CSS, pseudocoded JS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//set interval for the timer
+//event listeners for all buttons
+//store highscores in local storages
+//need array to show highscores and initials
+//score highscore from highest to lowest
+//clear highscores button should clear all values; look up documentation for how to delete
